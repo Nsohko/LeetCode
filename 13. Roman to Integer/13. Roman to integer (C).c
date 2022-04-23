c8b6d92ec9c75777f4072547ba29c6a191f27dd8
Nsohko <102672238+Nsohko@users.noreply.github.com>
2022-04-23 23:18:40 +0800

    Update 13. Roman to integer (C).c

--- 13. Roman to Integer/13. Roman to integer (C).c
+++ 13. Roman to Integer/13. Roman to integer (C).c
@@ -1,1 +1,70 @@
+#include <stdio.h>
+#include<string.h>
 
+struct key
+{
+char roman;
+int dec;
+}keys[7]={
+	{'I', 1},
+	{'V', 5},
+	{'X', 10},
+	{'L', 50},
+	{'C', 100},
+	{'D', 500},
+	{'M', 1000}
+	};
+	
+typedef struct key key;
+int romantointeger(char inp[]);
+
+int main() 
+{
+    char inp[] = "CII";
+    int out = romantointeger(inp);
+    printf("%d",out);
+    return 0;
+}
+
+int returndecimal(char rom)
+{
+    for (int i = 0; i<7;i++)
+    {
+        if (keys[i].roman==rom)
+	{
+	    return keys[i].dec;
+	}
+    }
+    return -1;
+}
+
+int romconverter(char inp[])
+{
+    int leng=strlen(inp);
+    int i=0;
+    int out=0;
+    while (i<leng)
+    {
+        int val1=returndec(inp[i]);
+	if(i+1<leng)
+        {
+	    int val2=returndec(inp[i+1]);
+	    if (val2>val1)
+	    {
+	        out+=(val2-val1);
+		i+=2;
+	    }
+	    else
+	    {
+	        out+=val1;
+		i++;
+	    }
+        }
+	else
+	{
+	    out+=val1;
+	    i++;
+	}
+    }
+    return out;
+}
