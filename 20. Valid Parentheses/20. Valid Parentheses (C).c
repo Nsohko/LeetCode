a1677ae31e6f8dea714f961725aa072b5904226a
Nsohko <102672238+Nsohko@users.noreply.github.com>
2022-04-23 20:37:05 +0800

    Add files via upload

---
+++ 20. Valid Parentheses/20. Valid Parentheses (C).c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include<stdbool.h>
+#include<string.h>
+
+
+bool isValidparenthesis(char * s)
+{
+    int leng = strlen(s);
+    char stack[leng + 1];
+    stack[0] = '\0';
+    int top = 0;
+    for (int i = 0; i < leng; i++)
+    {
+        char curval = s[i];
+        char topstack = stack[top];
+        if (curval ==' (' || curval == '[' || curval == '{')
+        {
+            top++;
+            stack[top] = curval;
+        }
+        else
+        {
+            switch(curval)
+            {
+				case ')':
+				  if (topstack != '(')
+				  {
+					  return false;
+				  }
+				  top--;
+				  break;
+				case ']':
+				  if (topstack != '[')
+				  {
+					  return false;
+				  }
+				  top--;
+                  break;
+				case '}':
+				  if (topstack != '{')
+				  {
+					return false;
+				  }
+				  top--;
+				  break;
+				default:
+				  return false;
+				  break;
+			}
+		}
+	}
+	if (top == 0)
+	{
+	    return true;
+	}
+	return false;
+}
+
+int main(void)
+{
+    char inp[] = "()[]{}";
+    bool out = isValidparenthesis(inp);
+    printf("%i",out);
+    return 0;
+}
