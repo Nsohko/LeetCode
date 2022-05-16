3bb83b48cba752d28a167208e03e6a892d59574e
Nsohko <102672238+Nsohko@users.noreply.github.com>
2022-05-16 13:20:30 +0800

    Update 204. Count Primes (C).c

--- 204. Count Primes/204. Count Primes (C).c
+++ 204. Count Primes/204. Count Primes (C).c
@@ -46,19 +46,29 @@ int countPrimes2(int n)
     memset(PrimeList, true, sizeof(PrimeList));
 
     // since 2 is the first prime number, we start with 2.
+    // in this loop, we essentially iterate through all the prime numbers (till the square root of n) and then set all multiples of that prime number to false
+    // we only need to iterate till the square root of n, because by the time we reach this value, all non prime numbers in up till n would have been marked already. This is because the first "unique" non prime multiple of any prime number,x, is x * x. Any previous multiple of x would also have to be a multiple of another prime (eg x * 2 or x * 3) and as such would be marked off already. Thus, when we reach a value of a prime number, i, such that i > sqrt(n) (which is equivalent to saying i * i > n), the first unique multiple of i that has not yet been marked is i * i, which exceeds n, and is hence not useful in our solution. All previous multiple of i, eg i * 2, i * 3, have already been marked when the loop was on 2 or 3, etc respectively. Thus, by the time I reach i = sqrt(n), all relevant non- primes would be marked alr
+    // the limit of i * i <= n can also be somewhat explained using the same logic as the isPrime function above, in that once i exceeds sqrt(n), we are checking the same expression, just that they are flipped 
     for(int i = 2; i * i <= n; i ++)
     {
+        // this checks whether it is prime. If it is marked as true in PrimeList at the point we come across it, it means the value cannot be expressed as a multiple of any smaller numbers. Thus, it must be prime
+        // if it is, we will have to mark out all the following multiples of this prime number as false, for future reference 
         if (PrimeList[i])
         {
+            // As written above, the first new "unique" multiple of a prime number, that has not already been marked, is i * i, where i is a prime number. All previous multiples of i  can be expressed as i * c, where c is a combination of previous prime numbers. Thus, when the loop was on those previous prime numbers, this multiple would be marked as false already. For example, consider the multiple i * 2. This multiple would have already been marked as false when the loop was on 2. Thus, we do not need to check it again, and can instead start at i * i, which is the first new unmarked multiple
+            // we keep iterating till we reach the last number,n
+            // after every cycle, we increment multiple by i, ie we sat multiple = multiple + i. This is because this will directly give us the value of the next multiple of i
             for (int multiple = i * i; multiple < n; multiple = multiple + i)
             {
+                // since multiple is a multiple of a prime number, it is not a prime number itself. Thus, we set it to False
                 PrimeList[multiple] = false;
             }
         }
     }
     
     int count = 0;
-    
+
+    // Finally, we iterate through the entire list, and count the number of times we see a box marked as true. These boxes correspond to a prime number and hence the number of boxes that are True is equal to the number of prime numbers
     for (int Prime = 2; Prime < n; Prime++)
     {
         if (PrimeList[Prime])
@@ -67,9 +77,9 @@ int countPrimes2(int n)
         }
     }
     return count;
-    
-    
 }
+
+// for more information on the above algorithm see the leetcode hint section for this problem.
 
 int main() {
     printf("%d", countPrimes2(2));
